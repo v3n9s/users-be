d34483a95b7e810aa1f4aca3887d6d2dc3cf5b60
Venyamin Shvets <venyashvets@gmail.com>
2022-08-03 17:18:46 +0300

    feat: don't reject unauthorized connections

--- src/db/instance.ts
+++ src/db/instance.ts
@@ -2,6 +2,9 @@ import postgres from 'postgres';
 import config from '../config';
 
 export const sql = postgres(config.DATABASE_CONNECTION_URL, {
+  ssl: {
+    rejectUnauthorized: false
+  },
   transform: {
     column: {
       from: postgres.toCamel,
